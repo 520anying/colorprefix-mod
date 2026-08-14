49d47a14b37c484dd3964a0a7c167f9af97d2475
520anying <2451873883@qq.com>
2026-08-14 17:18:47 +0800

    Update TimeOfSacrifice.java

--- src/main/java/net/undertaker/colorprefix/TimeOfSacrifice.java
+++ src/main/java/net/undertaker/colorprefix/TimeOfSacrifice.java
@@ -1,20 +1,14 @@
 package net.undertaker.colorprefix;
 
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.server.ServerAboutToStartEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
-@Mod("colorprefix")
+@Mod(TimeOfSacrifice.MODID)
 public class TimeOfSacrifice {
     public static final String MODID = "colorprefix";
 
     public TimeOfSacrifice() {
-        // 不在这里注册任何事件
-    }
-
-    @SubscribeEvent
-    public void onServerAboutToStart(ServerAboutToStartEvent event) {
         MinecraftForge.EVENT_BUS.register(new ChatHandler());
+        System.out.println("✅ 彩色称号模组已加载！");
     }
 }
